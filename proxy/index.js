5e2cfd39ddfbd8039921cc9033ea81d60b8efc81
Joachen D Busch <joachendbusch@gmail.com>
2019-10-02 13:17:22 -0700

    adding resources

--- proxy/index.js
+++ proxy/index.js
@@ -4,11 +4,13 @@ const superagent = require("superagent");
 const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("./public"));
+app.use("/", express.static("./public"));
 
 app.post("/url", (req, res) => {
+  console.log("the request is: " + req);
   superagent.get(req.body.url)
-    .then(html => res.status(200).send(html));
-})
+    .then(html => res.status(200).send(html.text))
+    .catch(function(error){console.error(error);});
+});
 
 app.listen(8080, console.log("boooooooo"));
